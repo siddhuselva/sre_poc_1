306dec6146aa03cd9c9e4205073c5391a2d6f137
siddhuselva <chitharthan.s@gmail.com>
2023-06-16 10:28:55 +0530

    initial commit

--- sample_app/app.py
+++ sample_app/app.py
@@ -4,10 +4,10 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # Configure MySQL connection
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'your_username'
-app.config['MYSQL_PASSWORD'] = 'your_password'
-app.config['MYSQL_DB'] = 'your_database_name'
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'srepoc123'
+app.config['MYSQL_DB'] = 'srepoc'
 
 mysql = MySQL(app)
 
--- sample_db/create_user_table.sql
+++ sample_db/create_user_table.sql
@@ -1,3 +1,5 @@
+USE srepoc;
+
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
